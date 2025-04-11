a4768370ae23df819d5e847ef4ffd6969092bc99
GrandEchoWhiskey <pv.wolf.eric@gmail.com>
2025-04-12 01:49:58 +0200

    fix overflow

--- display.py
+++ display.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
+import lcd
 
 WIDTH, HEIGHT = 128, 128
 
 class Display:
-    def __init__(self, lcd):
-        self.lcd = lcd
+    def __init__(self):
+        self.lcd = lcd.LCD()
         self.image = Image.new("RGB", (WIDTH, HEIGHT), "black")
         self.draw = ImageDraw.Draw(self.image)
         self.font = ImageFont.load_default()  # or use truetype if you like
