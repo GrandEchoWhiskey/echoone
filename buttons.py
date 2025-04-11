fdd3c807ffaa811d369893b493047197247cf1a7
GrandEchoWhiskey <pv.wolf.eric@gmail.com>
2025-04-11 13:08:38 +0200

    bouncetime debugging

--- buttons.py
+++ buttons.py
@@ -7,11 +7,12 @@ import time
 def callback(channel: int) -> None:
     """Callback function for button press event."""
     if channel in KEY_PINS.values():
+        # todo add lock to prevent multiple callbacks
         name = [key for key, pin in KEY_PINS.items() if pin == channel][0]
         print(f"Button {name} pressed.")
 
 def setup_buttons() -> None:
     """Set up GPIO buttons."""
     for pin in KEY_PINS.values():
-        GPIO.add_event_detect(pin, GPIO.FALLING, bouncetime=500)
+        GPIO.add_event_detect(pin, GPIO.FALLING, bouncetime=20)
         GPIO.add_event_callback(pin, callback=callback)
