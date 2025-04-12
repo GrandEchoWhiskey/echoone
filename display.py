5523c980a85290af5ba907ba2ffd24ede8ddc123
GrandEchoWhiskey <pv.wolf.eric@gmail.com>
2025-04-12 17:08:15 +0200

    try fixes

--- display.py
+++ display.py
@@ -1,17 +1,15 @@
 from PIL import Image, ImageDraw, ImageFont
 import lcd
 
-WIDTH, HEIGHT = 128, 128
-
 class Display:
     def __init__(self):
         self.lcd = lcd.LCD()
-        self.image = Image.new("RGB", (WIDTH, HEIGHT), "black")
+        self.image = Image.new("RGB", (self.lcd.width, self.lcd.height), "black")
         self.draw = ImageDraw.Draw(self.image)
         self.font = ImageFont.load_default()  # or use truetype if you like
 
     def clear(self):
-        self.draw.rectangle((0, 0, WIDTH, HEIGHT), fill="black")
+        self.draw.rectangle((0, 0, self.lcd.width, self.lcd.height), fill="black")
 
     def show(self):
         self.lcd.showImage(self.image)
@@ -23,7 +21,7 @@ class Display:
 
         for i, option in enumerate(options):
             y = 15 + i * 12
-            if y < HEIGHT - 10:  # Avoid bottom overflow
+            if y < self.lcd.height - 10:  # Avoid bottom overflow
                 prefix = ">" if i == selected else " "
                 color = "cyan" if i == selected else "white"
                 self.draw.text((10, y), f"{prefix} {option}", font=self.font, fill=color)
